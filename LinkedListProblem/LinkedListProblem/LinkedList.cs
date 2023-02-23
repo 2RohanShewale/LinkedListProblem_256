b80050f90a580d8a9088d8fc93a162bcb20b978b
Rohan Shewale <shewalerohan50@gmail.com>
2023-02-23 12:14:10 +0530

    [Rohan]Add: Create Simple Linked list

---
+++ LinkedListProblem/LinkedListProblem/LinkedList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblem
+{
+    internal class LinkedList
+    {
+        public Node head;
+
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = this.head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine($"\n{node.data} is added to the list.");
+        }
+
+        public void Display()
+        {
+            Node temp= this.head;
+            if (head != null)
+            {
+                Console.WriteLine("\nItems in list are: ");
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data);
+                    temp= temp.next;
+                }
+            }
+        }
+    }
+}
---
+++ LinkedListProblem/LinkedListProblem/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            while (true)
+            {
+                Console.WriteLine("*------Linked List------*");
+                Console.WriteLine("\nOptions: ");
+                Console.Write("1.Add elements\n2.Display: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        linkedList.Add(56);
+                        linkedList.Add(30);
+                        linkedList.Add(70);
+                        break;
+                    case 2:
+                        linkedList.Display();
+                        break;
+                    default:
+                        Console.WriteLine("type correct value");
+                        break;
+                }
+
+            }
+        }
+    }
+}
